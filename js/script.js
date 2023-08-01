8e1c4677373aedfbe4b105d0ca4424b05ba6d13b
ash_c <campbell.ashley735@gmail.com>
2023-08-01 17:09:09 -0500

    Added click event for more info

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 //* Global Variables *//
-//* Selecting Overview div class *//
 const overview = document.querySelector(".overview");
 
 const username = "ashc735";
 
 const repoList = document.querySelector(".repo-list");
+
+const allReposContainer = document.querySelector(".repos");
+
+const repoData = document.querySelector(".repo-data"); 
+
+//* Async functions calling user data *//
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -46,4 +51,42 @@ const displayRepos = function (repos) {
     }
 };
 
+repoList.addEventListener("click", function (e) {
+    if (e.target.matches("h3")) {
+      const repoName = e.target.innerText;
+      getRepoInfo(repoName);
+    }
+  });
 
+const getRepoInfo = async function (repoName) {
+    const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await fetchInfo.json();
+    console.log(repoInfo);
+    //* Grab languages *//
+    const fetchLanguages = await fetch(repoInfo.languages_url);
+    const languageData = await fetchLanguages.json();
+  
+    //* Make a list of languages *//
+    const languages = [];
+    for (const language in languageData) {
+      languages.push(language);
+    }
+  
+    displayRepoInfo(repoInfo, languages);
+  };
+  
+
+const displayRepoInfo = function (repoInfo, languages) {
+    repoData.innerHTML = "";
+    repoData.classList.remove("hide");
+    allReposContainer.classList.add("hide");
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <h3>Name: ${repoInfo.name}</h3>
+      <p>Description: ${repoInfo.description}</p>
+      <p>Default Branch: ${repoInfo.default_branch}</p>
+      <p>Languages: ${languages.join(",")}</p>
+      <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+    `;
+    repoData.append(div);
+  };
